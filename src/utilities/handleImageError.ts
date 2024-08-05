f3d445abccf881e28bbbe92cf4cacf9f7cea4f4e
Julio Aguilar <dev.julioaguilar@gmail.com>
2024-08-05 09:51:51 -0600

    refactor: :recycle: Remove unused utility functions for time formatting and image error handling

--- src/utilities/handleImageError.ts
+++
@@ -1,7 +0,0 @@
-import NotFoundImage from '../assets/imageNotFound.png';
-
-export const handleImageError = (
-  event: React.SyntheticEvent<HTMLImageElement, Event>
-) => {
-  event.currentTarget.src = NotFoundImage;
-};
